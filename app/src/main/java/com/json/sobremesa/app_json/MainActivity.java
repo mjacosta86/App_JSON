9eb6f493f2ac3b834bd8ebe84356751d81c388d4
sobremesa <manueljoseacosta86@gmail.com>
2015-07-10 15:26:52 +0200

    Primera versión app futbol JSON

---
+++ app/src/main/java/com/json/sobremesa/app_json/MainActivity.java
@@ -0,0 +1,254 @@
+package com.json.sobremesa.app_json;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final String ACTIVITY = "MainActivity";
+    ProgressDialog progress_dialog;
+    String tz = "Europe/Madrid";
+    String format = "json";
+    String req = "matchsday";
+    String key = "9fb0641102eeb3cd7d7db99606624a08";
+    String date = "2015-7-9"; //año-mes-dia
+    ArrayList<MatchsDay> lista_partidos;
+    TextView texto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final String METODO = "onCreate()";
+        Log.i(ACTIVITY, METODO);
+        new PoisJSON().execute("http://www.resultados-futbol.com/scripts/api/api.php?tz=" + tz + "&format=" + format + "&req=" + req + "&key=" + key + "&date=" + date);
+
+        texto = (TextView)findViewById(R.id.tvTexto);
+
+
+    }
+    protected void onStart()
+    {
+        final String METODO = "onStart()";
+        Log.i(ACTIVITY, METODO);
+        super.onStart();
+
+
+
+    }
+    protected void onRestart()
+    {
+        final String METODO = "onRestart()";
+        Log.i(ACTIVITY, METODO);
+        super.onRestart();
+
+
+
+    }
+    protected void onResume()
+    {
+        final String METODO = "onResume()";
+        Log.i(ACTIVITY, METODO);
+        super.onResume();
+
+
+
+    }
+    protected void onPause()
+    {
+        final String METODO = "onPause()";
+        Log.i(ACTIVITY,METODO);
+        super.onPause();
+
+    }
+    protected void onStop()
+    {
+        final String METODO = "onStop()";
+        Log.i(ACTIVITY, METODO);
+        super.onStop();
+
+    }
+    protected void onDestroy()
+    {
+        final String METODO = "onDestroy()";
+        Log.i(ACTIVITY,METODO);
+        super.onDestroy();
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private class PoisJSON extends AsyncTask<String, Void, String> //Hilo para descargar el JSON
+    {
+        @Override
+        protected void onPreExecute()
+        {
+            final String METODO = "onPreExecute()";
+            Log.i(ACTIVITY,METODO);
+            progress_dialog = new ProgressDialog(MainActivity.this); //Aqui lanzo una ventana para advertir que se están descargando el JSON de POIS
+            progress_dialog.setMessage("Descargando archivos...");
+            progress_dialog.setCancelable(false);
+            progress_dialog.show();
+        }
+        protected String doInBackground(String... urls)
+        {
+            final String METODO = "doInBackground()";
+            Log.i(ACTIVITY,METODO);
+            return readJSONFeed(urls[0]); //Desde aquí llamo al método que descarga el JSON
+        }
+
+        protected void onPostExecute(String result)
+        {
+            final String METODO = "onPostExecute()";
+            Log.i(ACTIVITY, METODO);
+            try
+            {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray pois = new JSONArray(jsonObject.getString("matches")); //Aqui extraigo lo que contiene el array de JSON "list"
+
+                //---print out the content of the json feed---
+                lista_partidos = new ArrayList<MatchsDay>();
+                for (int x = 0; x < pois.length(); x++) //En este bucle introduzco los datos en los vectores correspondientes
+                {
+                    JSONObject poisItem = pois.getJSONObject(x);
+                    MatchsDay partido = new MatchsDay();
+                    partido.setId(poisItem.getString("id").toString().trim());
+                    Log.i(ACTIVITY, METODO+" ID:"+partido.getId());
+                    partido.setRound(poisItem.getString("round").toString().trim());
+                    partido.setLocal(poisItem.getString("local").toString().trim());
+                    partido.setVisitor(poisItem.getString("visitor").toString().trim());
+                    partido.setCompetitionName(poisItem.getString("competition_name").toString().trim());
+                    partido.setLeagueId(poisItem.getString("league_id").toString().trim());
+                    partido.setTeam1(poisItem.getString("team1").toString().trim());
+                    partido.setTeam2(poisItem.getString("team2").toString().trim());
+                    partido.setNumc(poisItem.getString("numc").toString().trim());
+                    partido.setYear(poisItem.getString("year").toString().trim());
+                    partido.setPlayoffs(poisItem.getString("playoffs").toString().trim());
+                    partido.setCoef(poisItem.getString("coef").toString().trim());
+                    partido.setCflag(poisItem.getString("cflag").toString().trim());
+                    partido.setLocalShield(poisItem.getString("local_shield").toString().trim());
+                    partido.setVisitorShield(poisItem.getString("visitor_shield").toString().trim());
+                    partido.setDate(poisItem.getString("date").toString().trim());
+                    partido.setHour(poisItem.getString("hour").toString().trim());
+                    partido.setMinute(poisItem.getString("minute").toString().trim());
+                    partido.setResult(poisItem.getString("result").toString().trim());
+                    partido.setLiveMinute(poisItem.getString("live_minute").toString().trim());
+                    partido.setStatus(poisItem.getString("status").toString().trim());
+                    partido.setChannels(poisItem.getString("channels").toString().trim());
+
+                    lista_partidos.add(partido);
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Log.d("ReadPlacesFeedTask", e.getLocalizedMessage());
+            }
+
+            progress_dialog.dismiss(); //Cierro el cuadro de dialogo
+
+            if(lista_partidos.size()<=0)
+            {
+                Log.w(ACTIVITY, METODO + " La lista está vacía");
+            }
+            else
+            {
+                Log.w(ACTIVITY, METODO + " La lista no está vacía");
+                for(int i = 0; i < lista_partidos.size(); ++i)
+                {
+                    String partido = " ID:" + lista_partidos.get(i).getId()+"\n"
+                            +"Local:" + lista_partidos.get(i).getLocal()+"\n"
+                            +"Visitante:" + lista_partidos.get(i).getVisitor();
+                    Log.i(ACTIVITY, METODO+" "+partido);
+
+                    texto.setText(texto.getText()+"\n"+partido);
+
+                }
+
+
+
+            }
+            /*Intent i = new Intent(MainActivity.this, Principal.class); //Paso a la siguiente Activity pasando los datos necesarios para mostrar una lista de puntos y el mapa con los puntos
+            startActivity(i);
+            finish();*/
+
+        }
+    }
+    public String readJSONFeed(String URL) //Metodo para descargar el JSON
+    {
+        final String METODO = "readJSONFeed()";
+        Log.i(ACTIVITY,METODO);
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(URL);
+        try
+        {
+            HttpResponse response = httpClient.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200)
+            {
+                HttpEntity entity = response.getEntity();
+                InputStream inputStream = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null)
+                {
+                    stringBuilder.append(line);
+                }
+                inputStream.close();
+            }
+            else
+            {
+                Log.d(ACTIVITY, METODO+" Failed to download file");
+            }
+        }
+        catch (Exception e)
+        {
+            Log.d(ACTIVITY,METODO+" readJSONFeed "+ e.getLocalizedMessage());
+        }
+        return stringBuilder.toString();
+    }
+
+}
---
+++ app/src/main/java/com/json/sobremesa/app_json/MatchsDay.java
@@ -0,0 +1,303 @@
+package com.json.sobremesa.app_json;
+
+import java.io.Serializable;
+
+/**
+ * Created by sobremesa on 09/07/2015.
+ */
+public class MatchsDay implements Serializable
+{
+    /*
+    [id] => 1322927
+    [round] => 12
+    [local] => Goiás EC
+    [visitor] => Santos FC
+    [competition_name] => Liga Brasileña
+    [league_id] => 11071
+    [team1] => 173845
+    [team2] => 173853
+    [numc] => 130
+    [year] => 2015
+    [playoffs] =>
+    [group_code] => 1
+    [coef] => 45.000
+    [cflag] => http://cdn.popuz.com/media/img/flags/st3/small/br.png
+    [local_shield] => http://thumb.resfu.com/img_data/escudos/medium/1245.jpg?size=60x
+    [visitor_shield] => http://thumb.resfu.com/img_data/escudos/medium/2270.jpg?size=60x
+    [date] => 2015/07/09
+    [hour] => 00
+    [minute] => 30
+    [result] => 4-1
+    [live_minute] =>
+    [status] => 1
+    [channels] => Array
+     */
+    private static final long serialVersionUID = 1566632478541064056L;
+    private String id;                  //ID de partido
+    private String round;               //Jornada de partido
+    private String local;               //Equipo Local
+    private String visitor;             //Equipo Visitante
+    private String competition_name;    //Nombre de la competici�n
+    private String league_id;           //Id de la competicion
+    private String team1;               //Id equipo local
+    private String team2;               //Id equipo visitante
+    private String numc;
+    private String year;
+    private String playoffs;
+    private String group_code;
+    private String coef;
+    private String cflag;               //Bandera
+    private String local_shield;        //Escudo equipo local
+    private String visitor_shield;      //Escudo equipo visitante
+    private String date;                //Fecha
+    private String hour;                //Hora
+    private String minute;              //Minuto
+    private String result;              //Resultado
+    private String live_minute;         //Minuto actual
+    private String status;
+    private String channels;
+
+    public MatchsDay(){}
+
+    public MatchsDay(String i,
+                       String ro,
+                       String lo,
+                       String vi,
+                       String co,
+                       String le,
+                       String t1,
+                       String t2,
+                       String nu,
+                       String ye,
+                       String pl,
+                       String gp,
+                       String coe,
+                       String cf,
+                       String ls,
+                       String vs,
+                       String da,
+                       String ho,
+                       String mi,
+                       String re,
+                       String li,
+                       String st,
+                       String ch)
+    {
+        this.id = i;
+        this.round=ro;
+        this.local=lo;
+        this.visitor = vi;
+        this.competition_name = co;
+        this.league_id = le;
+        this.team1 = t1;
+        this.team2 = t2;
+        this.numc = nu;
+        this.year = ye;
+        this.playoffs = pl;
+        this.group_code = gp;
+        this.coef = coe;
+        this.cflag = cf;
+        this.local_shield = ls;
+        this.visitor_shield = vs;
+        this.date = da;
+        this.hour = ho;
+        this.minute = mi;
+        this.result = re;
+        this.live_minute = li;
+        this.status = st;
+        this.channels = ch;
+
+
+    }
+    //Getters
+    public String getId()
+    {
+        return id;
+    }
+    public String getRound()
+    {
+        return round;
+    }
+    public String getLocal()
+    {
+        return local;
+    }
+    public String getVisitor()
+    {
+        return visitor;
+    }
+    public String getCompetitionName()
+    {
+        return competition_name;
+    }
+    public String getLeagueId()
+    {
+        return league_id;
+    }
+    public String getTeam1()
+    {
+        return team1;
+    }
+    public String getTeam2()
+    {
+        return team2;
+    }
+    public String getNumc()
+    {
+        return numc;
+    }
+    public String getYear()
+    {
+        return year;
+    }
+    public String getPlayoffs()
+    {
+        return playoffs;
+    }
+    public String getGroupCode()
+    {
+        return group_code;
+    }
+    public String getCoef()
+    {
+        return coef;
+    }
+    public String getCflag()
+    {
+        return cflag;
+    }
+    public String getLocalShield()
+    {
+        return local_shield;
+    }
+    public String getVisitorShield()
+    {
+        return visitor_shield;
+    }
+    public String getDate()
+    {
+        return date;
+    }
+    public String getHour()
+    {
+        return hour;
+    }
+    public String getMinute()
+    {
+        return minute;
+    }
+    public String getResult()
+    {
+        return result;
+    }
+    public String getLiveMinute()
+    {
+        return live_minute;
+    }
+    public String getStatus()
+    {
+        return status;
+    }
+    public String getChannels()
+    {
+        return channels;
+    }
+
+    //Setters
+    public void setId(String i)
+    {
+        this.id = i;
+    }
+    public void setRound(String ro)
+    {
+        this.round = ro;
+    }
+    public void setLocal(String lo)
+    {
+        this.local = lo;
+    }
+    public void setVisitor(String vi)
+    {
+        this.visitor = vi;
+    }
+    public void setCompetitionName(String co)
+    {
+        this.competition_name = co;
+    }
+    public void setLeagueId(String le)
+    {
+        this.league_id = le;
+    }
+    public void setTeam1(String t1)
+    {
+        this.team1 = t1;
+    }
+    public void setTeam2(String t2)
+    {
+        this.team2 = t2;
+    }
+    public void setNumc(String nu)
+    {
+        this.numc = nu;
+    }
+    public void setYear(String ye)
+    {
+        this.year = ye;
+    }
+    public void setPlayoffs(String pl)
+    {
+        this.playoffs = pl;
+    }
+    public void setGroupCode(String gp)
+    {
+        this.group_code = gp;
+    }
+    public void setCoef(String coe)
+    {
+        this.coef = coe;
+    }
+    public void setCflag(String cl)
+    {
+        this.cflag = cl;
+    }
+    public void setLocalShield(String ls)
+    {
+        this.local_shield = ls;
+    }
+    public void setVisitorShield(String vs)
+    {
+        this.visitor_shield = vs;
+    }
+    public void setDate(String da)
+    {
+        this.date = da;
+    }
+    public void setHour(String ho)
+    {
+        this.hour = ho;
+    }
+    public void setMinute(String mi)
+    {
+        this.minute = mi;
+    }
+    public void setResult(String re)
+    {
+        this.result = re;
+    }
+    public void setLiveMinute(String lm)
+    {
+        this.live_minute = lm;
+    }
+    public void setStatus(String st)
+    {
+        this.status = st;
+    }
+    public void setChannels(String ch)
+    {
+        this.channels = ch;
+    }
+
+
+
+
+}
